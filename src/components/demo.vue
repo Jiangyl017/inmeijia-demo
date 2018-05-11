246a430c3ee38b3d49d35202352628b0f2f82194
jyl <602908219@qq.com>
2018-05-11 18:12:24 +0800

    写了一点吃饭去了

---
+++ src/components/demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+      <h1>demo</h1>
+      <flexbox>
+          <swiper></swiper>
+      </flexbox>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{
+
+      }
+  },
+  methods:{
+
+  }
+}
+</script>
+<style lang="less">
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello";
+import demo from "@/components/demo";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+    routes: [
+        {
+            path: "/",
+            redirect: "/demo"
+        },
+        {
+            path: "/demo",
+            name: "demo",
+            component: demo
+        },
+        {
+            path: "/hello",
+            name: "Hello",
+            component: Hello
+        }
+    ]
+});
